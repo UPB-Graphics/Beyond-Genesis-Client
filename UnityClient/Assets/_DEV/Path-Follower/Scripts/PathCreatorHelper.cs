7041b9c62e3674ab32df15b69c76cd839a2a1863
Robert <soarerobert17@gmail.com>
2022-02-13 19:20:51 +0200

    Add path follower

---
+++ UnityClient/Assets/_DEV/Path-Follower/Scripts/PathCreatorHelper.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PathCreatorHelper : MonoBehaviour
+{
+    public bool displayPath = true;
+    public bool autoRefreshWhenChildrenChange = false;
+    public Color pathColor = Color.magenta;
+    public Color pointColor = Color.gray;
+    public Color startingPointColor = Color.green;
+    public Color endingPointColor = Color.red;
+
+    [SerializeField] private PathCreator pathCreator;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        pathCreator = GetComponent<PathCreator>();
+        UpdateColor();
+    }
+
+    private void Update()
+    {
+        if (displayPath && pathCreator != null)
+        {
+            DisplayPath();
+        }
+    }
+
+    private void OnTransformChildrenChanged()
+    {
+        if (autoRefreshWhenChildrenChange && pathCreator != null)
+        {
+            pathCreator.points = GetComponentsInChildren<PathPoint>().ToList();
+        }
+        UpdateColor();
+    }
+
+    private void DisplayPath()
+    {
+        var points = pathCreator.points;
+        for (var i = 0; i < points.Count - 1; i++)
+        {
+            Debug.DrawLine(points[i].transform.position, points[i + 1].transform.position, pathColor);
+        }
+    }
+
+    private void UpdateColor()
+    {
+        for (var i = 0; i < pathCreator.points.Count; i++)
+        {
+            if (i == 0)
+            {
+                pathCreator.points[i].color = startingPointColor;
+            } else if (i == pathCreator.points.Count - 1)
+            {
+                pathCreator.points[i].color = endingPointColor;
+            }
+            else
+            {
+                pathCreator.points[i].color = pointColor;
+            }
+        }
+    }
+}
---
+++ UnityClient/Assets/_DEV/Path-Follower/Scripts/Testing/Move.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField] private PathFollower pathFollower;
+
+    private void Start()
+    {
+        pathFollower = GetComponent<PathFollower>();
+    }
+
+    private void Update()
+    {
+        var pos = transform.position;
+        var dir = pathFollower.GetDirection(pos);
+        if (dir != Vector3.zero)
+        {
+            var nextPosition = pos + dir * speed;
+            transform.position = Vector3.SmoothDamp(pos, nextPosition, ref velocity, smoothTime);
+        }
+    }
+}
