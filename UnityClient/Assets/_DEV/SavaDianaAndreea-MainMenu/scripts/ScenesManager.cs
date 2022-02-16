659492a7928949ffdd86cf415c6f786b7ba19ad7
dianaandreea03 <diana_andreea.sava@stud.acs.upb.ro>
2022-02-16 19:15:17 +0200

    Added Main Menu

---
+++ UnityClient/Assets/_DEV/SavaDianaAndreea-MainMenu/scripts/ScenesManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ScenesManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadNextScene() {
+        SceneManager.LoadScene("Game");
+    }
+    public void QuitGame() {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
