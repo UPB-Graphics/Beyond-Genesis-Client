0bd44047c981413e8e218881930f9497ed9a760f
ParaschivaMihai <mihaip226@gmail.com>
2022-02-18 21:45:02 +0200

    Implemented Vehicle System

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This repo is using GIT LFS (Large File System).
 
 ## Contributing
 Fork repo and read [contributing](https://github.com/UPB-Graphics/Beyond-Genesis-Client/blob/main/CONTRIBUTING.md) documentation.
+
+
---
+++ UnityClient/Assets/Vehicle System/Scripts/VehicleControlModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleControlModel 
+{
+    // Start is called before the first frame update
+    public Vector3 Direction { get; set; } //normalized vector to point in the direction we want to move the vechicle
+    public Quaternion Rotation { get; set; }
+    public float Force { get; set; }
+}
